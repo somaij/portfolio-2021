bd218f7a3c624d151bddd22acdf1c6bfcc066ad8
Jason Somai <jsomai458@gmail.com>
2020-12-31 14:21:43 -0500

    mobile styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <main>{children}</main>
         <footer>
-          Jason Somai © {new Date().getFullYear()}, Built with
+          Jason Somai © {new Date().getFullYear()}. <br class="d-block d-lg-none"/> Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a> and <a href="https://www.netlifycms.org/">Netlify CMS</a>.
         </footer>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
 import React from "react"
-
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BlogRoll from '../components/BlogRoll'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Helmet>
+    <title>404 Page Not Found | Jason Somai</title>
+  </Helmet>
+    <div className="page-404">
+    <div className="container">
+    <div className="row">
+      <div className='col-12 text-center'>
+      <h1>404: Not Found</h1>
+    <p>Try returning to my <a href="/">homepage</a> to get to where you were trying to go to.</p>
+    <p>Or, check out one of these blogs!</p>
+      </div>
+      <BlogRoll/>
+    </div>
+    </div>
+    </div>
   </Layout>
 )
 
