9bc5d42c21bab8255fc8ecc2d25d215950650a93
Jason Somai <jsomai458@gmail.com>
2021-01-01 15:35:05 -0500

    removed kebab on summary buttons

--- src/components/templates/WorkPost.js
+++ src/components/templates/WorkPost.js
@@ -70,7 +70,7 @@ const WorkPost = ({ data }) => {
             <h1>{data.markdownRemark.frontmatter.title}</h1>
             <div className="regular-text"><ReactMarkdown>{data.markdownRemark.frontmatter.summary}</ReactMarkdown></div>
             {data.markdownRemark.frontmatter.summary_buttons.map((button, i) => [
-            <a key={i} id={"btn-" + i} href={convertToKebabCase(button.btnlink)}>{button.btntext}
+            <a key={i} id={"btn-" + i} href={button.btnlink}>{button.btntext}
           </a>
           ])}
             </div>
