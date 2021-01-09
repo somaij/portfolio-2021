5add561d5ee0da4d73a7e811f0fb6ed6fe8a8a11
Jason Somai <jsomai458@gmail.com>
2021-01-09 16:50:41 -0500

    favicon try 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,10 +71,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#5E7CE2`,
+        theme_color: `#5E7CE2`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-starter.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
